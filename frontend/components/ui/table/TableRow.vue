aa6ca7ae3d1ff42de630e3630ecc893fcc4a01b7
Alexis <alexislafrance42@outlook.fr>
2024-12-13 11:07:25 +0100

    rename client-red-tetris to frontend

---
+++ frontend/components/ui/table/TableRow.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { cn } from '@/utils/index.js';
+
+const props = defineProps({
+  class: { type: null, required: false },
+});
+</script>
+
+<template>
+  <tr
+    :class="
+      cn(
+        'border-b transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted',
+        props.class,
+      )
+    "
+  >
+    <slot />
+  </tr>
+</template>
